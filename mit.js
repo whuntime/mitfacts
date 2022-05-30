dc451c117d7aad6c2417621117b7df18b973604e
whuntime <whuntime@gmail.com>
2022-05-30 13:10:01 -0400

    Updated school color test

--- mit.js
+++ mit.js
@@ -1,6 +1,6 @@
 var mit = {
     city: 'Cambridge',
-    colors: ['Black', 'Silver Gray', 'Cardinal', ''],
+    colors: ['Black', 'Silver Gray', 'Cardinal'],
     mascot: "Tim the Beaver",
     founded: 1861,
     motto: 'Mens et Manus',
