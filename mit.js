dad8a0cb2e540174cab285737c5db8a2b01b8ada
whuntime <whuntime@gmail.com>
2022-05-30 13:08:39 -0400

    Updated school mascot test

--- mit.js
+++ mit.js
@@ -1,7 +1,7 @@
 var mit = {
     city: 'Cambridge',
     colors: ['Black', 'Silver Gray', 'Cardinal', ''],
-    mascot: "robot",
+    mascot: "Tim the Beaver",
     founded: 1861,
     motto: 'Mens et Manus',
 };
